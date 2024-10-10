caf03edacad802440bfb5816ab29d8b397f9621f
Alen <alensabu12xtz@gmail.com>
2024-10-10 11:45:38 +0530

    clearing the issues

--- src/pages/About/Maps.jsx
+++ src/pages/About/Maps.jsx
@@ -6,7 +6,7 @@ import L from 'leaflet';
 // Fix for marker icons not displaying
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
