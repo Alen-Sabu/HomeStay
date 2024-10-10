ca38f562a8f3333b6479c18b73837710d4f817dc
Alen <alensabu12xtz@gmail.com>
2024-10-10 11:43:14 +0530

    clearing map issues

--- src/pages/About/Maps.jsx
+++ src/pages/About/Maps.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for marker icons not displaying
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const MapComponent = () => {
   const position = [9.770004, 77.090713];
@@ -11,7 +21,7 @@ const MapComponent = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker position={position}>
-        <Popup>A pretty marker here!</Popup>
+        <Popup>Our Location</Popup>
       </Marker>
     </MapContainer>
   );
